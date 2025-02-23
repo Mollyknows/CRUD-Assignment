8ec92f574551f9de687af106c599a6afa3056c7f
Molly Calhoun <mollyrose.calhoun@gmail.com>
2025-02-22 21:49:50 -0500

    fixing sql formatting

--- todo_table.sql
+++ todo_table.sql
@@ -1,10 +1,10 @@
 CREATE DATABASE IF NOT EXISTS `TODO_List` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
--- USE `TODO_List`;
+USE `TODO_List`;
 
--- CREATE TABLE IF NOT EXISTS `List` (
--- 	`id` INT(11) NOT NULL AUTO_INCREMENT,
---     `task` varchar(255) NOT NULL,
---     `deadline` varchar(255) NOT NULL,
---     `complete` BOOLEAN NOT NULL,
---     PRIMARY KEY (`id`)
--- ) ENGINE InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
+CREATE TABLE IF NOT EXISTS `List` (
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+    `task` varchar(255) NOT NULL,
+    `deadline` varchar(255) NOT NULL,
+    `complete` BOOLEAN NOT NULL,
+    PRIMARY KEY (`id`)
+) ENGINE InnoDB AUTO_INCREMENT = 1 DEFAULT CHARSET = utf8;
